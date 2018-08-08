1f45c04b9d2d2aad87e16300f29c23639202118a
Talgorhythm <dia.99000@gmail.com>
2018-08-08 12:43:02 +0900

    Implemented search function

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,4 +4,12 @@ class Plan < ApplicationRecord
 	has_many :items
 
 	paginates_per 5
+
+# 	def self.search(search)
+# 		if search #Controllerのパラメーターが空の時
+# 			Plan.where('title LIKE(?)', "%#{search}%").includes(:user).order(created_at: :desc).page(params[:page])
+# 		else
+# 			Plan.includes(:user).order(created_at: :desc).page(params[:page])
+# 		end
+# 	end
 end
